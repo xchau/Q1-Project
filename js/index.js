3ee8173f20925fc51767919ff933b58064249f05
Minh Chau <xiu.chau@gmail.com>
2016-12-06 14:38:59 -0800

    Close #19 Fill in description content divs

--- js/index.js
+++ js/index.js
@@ -8,8 +8,7 @@
     $('.parallax').parallax();
   });
 
-  function createHotCard(deals) {
-    console.log(deals);
+  const createHotCard = function(deals) {
     // card element
     const $colDiv = $('<div>').addClass('col s3');
     const $cardDiv = $('<div>').addClass('card dealcard');
@@ -63,24 +62,24 @@
   }
 
   // POPULATE CONTENT DIV WITH LOCAL DEALS
-//   const $xhr = $.ajax({
-//     method: 'GET',
-//     url: 'https://api.sqoot.com/v2/deals?api_key=s3btbi&category_slugs=dining-nightlife,activities-events,retail-services&per_page=8&radius=10&location=47.598962,-122.333799',
-//     dataType: 'json'
-//   });
-//
-//   $xhr.done((data) => {
-//     if ($xhr.status !== 200) {
-//       return;
-//     }
-//     for (const location of data.deals) {
-//       // console.log(location.deal);
-//       createHotCard(location.deal);
-//     }
-//   });
-//
-//   $xhr.fail((err) => {
-//     console.error(err);
-//   });
-//
+  const $xhr = $.ajax({
+    method: 'GET',
+    url: 'https://api.sqoot.com/v2/deals?api_key=s3btbi&category_slugs=dining-nightlife,activities-events,retail-services&per_page=12&radius=10&location=47.598962,-122.333799',
+    dataType: 'json'
+  });
+
+  $xhr.done((data) => {
+    if ($xhr.status !== 200) {
+      return;
+    }
+    for (const location of data.deals) {
+      console.log(location.deal);
+      createHotCard(location.deal);
+    }
+  });
+
+  $xhr.fail((err) => {
+    console.error(err);
+  });
+
 })();
